7358af2f773f1d3c9d6dee66ca167625653210a5
shafiq-ahmed <shafiqahmd3@gmail.com>
2022-09-26 12:42:14 +0600

    Update Reaction.java

--- src/main/java/com/workspaceit/socialmediaproject/entity/Reaction.java
+++ src/main/java/com/workspaceit/socialmediaproject/entity/Reaction.java
@@ -6,26 +6,10 @@ import javax.persistence.*;
 
 @Entity(name = "reaction_table")
 public class Reaction {
-    public Post getPost() {
-        return post;
-    }
-
-    public void setPost(Post post) {
-        this.post = post;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    // reaction 1 for upvote and 0 for downvote
+    // reaction 1 for upvote, 2 for downvote and 0 for none
     @NotNull
     @Enumerated(EnumType.ORDINAL)
     private UserReact userReact;
@@ -49,4 +33,22 @@ public class Reaction {
     public void setUserReact(UserReact userReact) {
         this.userReact = userReact;
     }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
 }
