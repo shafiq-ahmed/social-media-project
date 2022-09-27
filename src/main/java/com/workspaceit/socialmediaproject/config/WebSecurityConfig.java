46f9b3c12a7ec5d6ffd6cc9b59b180fc4e071961
shafiq-ahmed <shafiqahmd3@gmail.com>
2022-09-27 12:28:07 +0600

    Er diagram and securityconfig updated

--- src/main/java/com/workspaceit/socialmediaproject/config/WebSecurityConfig.java
+++ src/main/java/com/workspaceit/socialmediaproject/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -32,13 +33,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         return daoAuthenticationProvider;
     }
-
+    /*
     //configure type of authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(getAuthenticationProvider());
     }
-
+*/
     //configure which routes can be accessed by which users
 
 
@@ -56,4 +57,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin().defaultSuccessUrl("/user/login/validation");
 
     }
+
+
+
+//    @Bean
+//    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests()
+//                .antMatchers("/user/login").permitAll()
+//                .antMatchers("/user/login/validation").permitAll()
+//                .antMatchers("/user/register").permitAll()
+//                .antMatchers("/user/addUser").permitAll()
+//                .antMatchers("/user/**").authenticated()
+//                .antMatchers("/post/**").authenticated()
+//                .antMatchers("/comment/**").authenticated()
+//                .antMatchers("/reaction/**").authenticated()
+//                .and().formLogin().defaultSuccessUrl("/user/login/validation");
+//        return http.build();
+//
+//    }
 }
